de0c4104e330b3e6c11660477dcc207b6ba37d01
Pranay Ghuge <pranayghug@gmail.com>
2025-06-06 23:14:55 -0400

    refactor: reorganize snek_object definitions and include necessary headers

--- MaS/sneknew.h
+++ MaS/sneknew.h
@@ -1,11 +1,43 @@
 #pragma once
-#include "snekobject.h"
-#include "vm.h"
 
-snek_object_t *new_snek_integer(vm_t *vm, int value);
-snek_object_t *new_snek_float(vm_t *vm, float value);
-snek_object_t *new_snek_string(vm_t *vm, char *value);
-snek_object_t *new_snek_vector3(
-    vm_t *vm, snek_object_t *x, snek_object_t *y, snek_object_t *z
-);
-snek_object_t *new_snek_array(vm_t *vm, size_t size);
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "stack.h"
+
+typedef struct SnekObject snek_object_t;
+
+typedef struct {
+  size_t size;
+  snek_object_t **elements;
+} snek_array_t;
+
+typedef struct {
+  snek_object_t *x;
+  snek_object_t *y;
+  snek_object_t *z;
+} snek_vector_t;
+
+typedef enum SnekObjectKind {
+  INTEGER,
+  FLOAT,
+  STRING,
+  VECTOR3,
+  ARRAY,
+} snek_object_kind_t;
+
+typedef union SnekObjectData {
+  int v_int;
+  float v_float;
+  char *v_string;
+  snek_vector_t v_vector3;
+  snek_array_t v_array;
+} snek_object_data_t;
+
+typedef struct SnekObject {
+  snek_object_kind_t kind;
+  snek_object_data_t data;
+  bool is_marked;
+} snek_object_t;
+
+void snek_object_free(snek_object_t *obj);
